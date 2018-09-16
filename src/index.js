08ed1db1333f23a33841797d3aae44b16ffe0741
ddmng <daniele.diminica@gmail.com>
2018-09-16 10:08:56 +0200

    Test dynamic font load

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import '/styles/style'
 
 const googleFontsUrl = "https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=AIzaSyBpJbJtNqviQ-PDH2a_mmvvKIeFY6jT2vk"
 
+var font = new FontFace(
+    'BioRhyme Expanded', 
+    'url(http://fonts.gstatic.com/s/biorhymeexpanded/v3/i7dQIE1zZzytGswgU577CDY9LjbffySU.ttf)', 
+    { weight: 'normal', style: 'normal' });
+
+font.load().then(function(loaded_face) {
+    console.log(loaded_face)
+    document.fonts.add(loaded_face);
+    document.body.style.fontFamily = 'BioRhyme Expanded';
+}).catch(function(error) {
+    console.log(error)
+});
+
+
 console.log("Started")
 
 const ChangeColor = (state, {color}) => ({
@@ -10,6 +24,27 @@ const ChangeColor = (state, {color}) => ({
     textStyle: {
         ...state.textStyle,
         color,
+        "font-family": font
+    }
+})
+
+const NextFont = (state, {font}) => ({
+    ...state,
+    textStyle: {
+        ...state.textStyle,
+        "font-family": font
+    }
+})
+
+const ChangeBackground = (state, {color}) => ({
+    ...state,
+    containerStyle: {
+        ...state.containerStyle,
+        "background-color": color
+    },
+    textStyle: {
+        ...state.textStyle,
+        "background-color": color
     }
 })
 
@@ -26,7 +61,9 @@ app({
     <div class="container" style={state.containerStyle}>
         <div class="subcontainer">
             <input class="text" value={state.text} style={state.textStyle}></input>
-            <button onClick={[ChangeColor, {color: "blue"}]}>Click me</button>
+            <button onClick={[NextFont, {font: "BioRhyme Expanded"}]}>Next font</button>
+            <button onClick={[ChangeColor, {color: "blue"}]}>Foreground color</button>
+            <button onClick={[ChangeBackground, {color: "gray"}]}>Background color</button>
         </div>
     </div>
     ,
